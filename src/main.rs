6e0e7acb8c06ef60d3bc64c4fc7d6481ff4a3102
milley <milley.gm@gmail.com>
2023-03-26 17:36:11 +0800

    refactor code: remove Router and Server to http directory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::{net::SocketAddr, sync::{Arc, RwLock}, collections::HashMap};
-
-use axum::{Router, routing::get, extract::{Query, State}, response::IntoResponse, Json, http::StatusCode};
-use serde::{Serialize, Deserialize};
+use std::{net::SocketAddr};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use uuid::Uuid;
+
+use todo_server::http;
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -15,66 +14,11 @@ async fn main() -> anyhow::Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let db = Db::default();
-
-    let app = Router::new()
-        .route("/todos", get(todos_index).post(todos_create))
-        .with_state(db);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+
+    http::serve(addr).await?;
     
     Ok(())
 }
-
-#[derive(Debug, Deserialize, Default)]
-pub struct Pagination {
-    pub offset: Option<usize>,
-    pub limit: Option<usize>,
-}
-
-async fn todos_index(
-    pagination: Option<Query<Pagination>>,
-    State(db): State<Db>
-) -> impl IntoResponse {
-    let todos = db.read().unwrap();
-
-    let Query(pagination) = pagination.unwrap_or_default();
-
-    let todos = todos
-        .values()
-        .skip(pagination.offset.unwrap_or(0))
-        .take(pagination.limit.unwrap_or(usize::MAX))
-        .cloned()
-        .collect::<Vec<_>>();
-
-    Json(todos)
-}
-
-async fn todos_create(State(db): State<Db>, Json(input): Json<CreateTodo>) -> impl IntoResponse {
-    let todo = Todo {
-        id: Uuid::new_v4(),
-        text: input.text,
-        completed: false,
-    };
-
-    db.write().unwrap().insert(todo.id, todo.clone());
-
-    (StatusCode::CREATED, Json(todo))
-}
-
-#[derive(Debug, Deserialize)]
-struct CreateTodo {
-    text: String,
-}
-
-type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
-
-#[derive(Debug, Serialize, Clone)]
-struct Todo {
-    id: Uuid,
-    text: String,
-    completed: bool,
-}
-
